8beb790a30b80f06c96240a4acfa337aae3af056
Iwaa Kusumah <107662442+iwaakusumah@users.noreply.github.com>
2023-12-09 16:22:42 +0700

    Update promo controller

--- src/controllers/promo.js
+++ src/controllers/promo.js
@@ -23,7 +23,7 @@ const addPromo = async (req, res) => {
         image
     });
 
-    const addPromo = await promo.save();
+    const addPromo = await Promo.save();
     res.status(201).json({ message: 'Promo created successfully', data: addPromo });
   } catch (error) {
     console.error(error);
